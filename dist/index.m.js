8333aec84a3d55204f0567541f2a0fe0e0cfe7b4
leastbad <hello@leastbad.com>
2021-09-29 23:47:48 -0400

    support stimulus 3.0

--- dist/index.m.js
+++ dist/index.m.js
@@ -117,10 +117,10 @@ function _createSuper(Derived) {
 }
 
 /*!
- * hotkeys-js v3.8.1
+ * hotkeys-js v3.8.7
  * A simple micro-library for defining and dispatching keyboard shortcuts. It has no dependencies.
  * 
- * Copyright (c) 2020 kenny wong <wowohoo@qq.com>
+ * Copyright (c) 2021 kenny wong <wowohoo@qq.com>
  * http://jaywcjlove.github.io/hotkeys
  * 
  * Licensed under the MIT license.
@@ -201,6 +201,22 @@ var _keyMap = {
   pageup: 33,
   pagedown: 34,
   capslock: 20,
+  num_0: 96,
+  num_1: 97,
+  num_2: 98,
+  num_3: 99,
+  num_4: 100,
+  num_5: 101,
+  num_6: 102,
+  num_7: 103,
+  num_8: 104,
+  num_9: 105,
+  num_multiply: 106,
+  num_add: 107,
+  num_enter: 108,
+  num_subtract: 109,
+  num_decimal: 110,
+  num_divide: 111,
   '⇪': 20,
   ',': 188,
   '.': 190,
